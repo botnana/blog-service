7b0d03358c9b68909bfbbce137e14eee7bd5a9ba
ccwu <ccwu660601@mapacode.tw>
2015-04-02 14:45:23 +0800

    CCWU: added author.

--- examples/posts/posts.js
+++ examples/posts/posts.js
@@ -8,18 +8,21 @@ var _posts = [
         title: 'My First Blog Post',
         md: 'first_post.md',
         published: '2015-02-25',
+        author: 'Sirius Wu',
         preview: 'Everyone has to start somewhere'
     },
     {
         title: 'I Blogged Again',
         md: 'blogged_again.md',
         published: '2015-02-25',
+        author: 'Sirius Wu',
         preview: 'I did it again.'
     },
     {
         title: 'Botbone 入門',
         md: 'botbone-getting-started.md',
         published: '2015-02-25',
+        author: 'Sirius Wu',
         preview: 'BotBone Getting Started Guide'
     }
 ];
