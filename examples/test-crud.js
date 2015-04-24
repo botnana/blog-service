b7b3306a90d6db30018c4d43d73b3f44c9ccd14b
ccwu <ccwu660601@mapacode.tw>
2015-04-24 19:24:14 +0800

    CCWU: implemented update and delete for blog.js.

--- examples/test-crud.js
+++ examples/test-crud.js
@@ -5,35 +5,74 @@
 
 var blogService = require('../index').fs(__dirname + '/posts/');
 var Fetcher = require('fetchr');
+var async = require('async');
 
 Fetcher.registerFetcher(blogService);
 var fetcher = new Fetcher();
 
-// Get list of blog and all of its contents.
-fetcher.create (
-    'blog',
-    {
-        "title": "Test",
-        "published": "2015-04-23",
-        "author": "Sirius Wu",
-        "price": "800",
-        "preview": "Test post...",
-        "tags": ["test"],
-        "marked": "* test"
-    }, {}, function (err, post1) {
-        if(err) {
-            console.log(err);
-        } else {
-            console.log('create post');
-            console.log(post1);
-            fetcher.read ('blog', {md: post1.md}, {}, function(err, post2) {
-                if(err) {
-                    console.log(err);
-                } else {
-                    console.log('read post');
-                    console.log(post2);
-                }
-            });
-        }
-    }
-);
+var post;
+
+async.series([
+    function(callback) {
+        fetcher.create ('blog', {
+            "title": "Test",
+            "published": "2015-04-23",
+            "author": "Sirius Wu",
+            "price": "800",
+            "preview": "Test post...",
+            "tags": ["test"],
+            "marked": "* test"
+        }, {}, function (err, post1) {
+            if(err) {
+                console.log(err);
+            } else {
+                console.log('create post');
+                console.log(post1);
+            }
+            post = post1;
+            callback(err, post1);
+        });
+    },
+    function(callback) {
+        fetcher.read ('blog', {md: post.md}, {}, function(err, post2) {
+            if(err) {
+                console.log(err);
+            } else {
+                console.log('read post');
+                console.log(post2);
+            }
+            callback(err, post2);
+        });
+    },
+    function(callback) {
+        fetcher.update ('blog', {
+            "md": post.md,
+            "title": "Test Again",
+            "published": "2015-04-24",
+            "author": "Cheng-Chang Wu",
+            "price": "700",
+            "preview": "Test post again...",
+            "tags": ["test", "again"],
+            "marked": "* test again"
+        }, {}, function (err, post3) {
+            if(err) {
+                console.log(err);
+            } else {
+                console.log('update post');
+                console.log(post3);
+            }
+            callback(err, post3);
+        });
+    },
+    function(callback) {
+        fetcher.delete ('blog', {"md": post.md}, {}, function(err, result) {
+            if(err) {
+                console.log(err);
+            } else {
+                console.log('delete post');
+                console.log(result);
+            }
+            callback(err, result);
+        });
+    },
+]);
