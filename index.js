51a455302ec739be226068eb02c9a7306646407a
ccwu <ccwu660601@mapacode.tw>
2015-04-28 23:06:29 +0800

    CCWU: added back fs and pg.

--- index.js
+++ index.js
@@ -6,6 +6,8 @@
 var blogfs = require('./lib/blog');
 var blogpg = require('./lib/blog-pg');
 module.exports = {
+    fs: blogfs,
+    pg: blogpg,
     configure: function (config) {
         if (config.module === "fs") {
             return blogfs(config.path);
